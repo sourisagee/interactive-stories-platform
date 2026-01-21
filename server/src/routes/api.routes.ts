1da783856f4c203f6cba605783d677726c472a54
sourisagee <serdeshko@icloud.com>
2026-01-21 18:48:00 +0300

    playthrough routes connected to api routes

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
+import { Router } from "express";
 // import userRouter from './user.routes';
 // import authRouter from './auth.routes';
 // import taskRouter from './task.routes';
+import playthroughRouter from "./playthrough.routes";
 
 const apiRouter = Router();
 
 // apiRouter.use('/auth', authRouter);
 // apiRouter.use('/users', userRouter);
 // apiRouter.use('/tasks', taskRouter);
+apiRouter.use("/playthrough", playthroughRouter);
 
 export default apiRouter;
