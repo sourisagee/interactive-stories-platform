5e2ff38a768d0bf10595415cdb84944011aee636
sourisagee <serdeshko@icloud.com>
2026-01-22 13:54:05 +0300

    choice routes updated

--- server/src/routes/choice.routes.ts
+++ server/src/routes/choice.routes.ts
@@ -1,29 +1,49 @@
-import { Router } from 'express';
-// import verifyAccessToken from '../middleware/verifyAccessToken';
-// import { ChoiceController } from '../controllers/choice.controller';
+import { Router } from "express";
+import verifyAccessToken from "../middleware/verifyAccessToken";
+import ChoiceController from "../controllers/choice.controller";
 
 const choiceRouter = Router();
 
 // ДЛЯ АВТОРОВ
 
 // Создать выбор
-// router.post('/', verifyAccessToken, ChoiceController.createChoice);
+choiceRouter.post("/", verifyAccessToken, ChoiceController.createChoice);
 
 // Обновить выбор
-// router.put('/:choiceId', verifyAccessToken, ChoiceController.updateChoice);
+choiceRouter.put(
+  "/:choiceId",
+  verifyAccessToken,
+  ChoiceController.updateChoice,
+);
 
 // Удалить выбор
-// router.delete('/:choiceId', verifyAccessToken, ChoiceController.deleteChoice);
+choiceRouter.delete(
+  "/:choiceId",
+  verifyAccessToken,
+  ChoiceController.deleteChoice,
+);
 
 // Получить все выборы для истории (для редактирования графа)
-// router.get('/story/:storyId', verifyAccessToken, ChoiceController.getAllChoicesForStory);
+choiceRouter.get(
+  "/story/:storyId",
+  verifyAccessToken,
+  ChoiceController.getAllChoicesForStory,
+);
 
 // ДЛЯ ИГРОКОВ
 
 // Получить выборы из узла (для прохождения)
-// router.get('/node/:nodeId', verifyAccessToken, ChoiceController.getChoicesFromNode);
+choiceRouter.get(
+  "/node/:nodeId",
+  verifyAccessToken,
+  ChoiceController.getChoicesFromNode,
+);
 
 // Получить информацию о выборе
-// router.get('/:choiceId', verifyAccessToken, ChoiceController.getChoiceById);
+choiceRouter.get(
+  "/:choiceId",
+  verifyAccessToken,
+  ChoiceController.getChoiceById,
+);
 
 export default choiceRouter;
