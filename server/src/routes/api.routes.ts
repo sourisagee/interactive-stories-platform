9f2ba2a5fa04c3da50911bf23ed7ab3eaac6a1f1
sourisagee <serdeshko@icloud.com>
2026-01-22 09:55:07 +0300

    choice routes

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import userRouter from './user.routes';
-import authRouter from './auth.routes';
+import userRouter from "./user.routes";
+import authRouter from "./auth.routes";
 import playthroughRouter from "./playthrough.routes";
+import choiceRouter from "./choice.routes";
 
 const apiRouter = Router();
-  
-apiRouter.use('/auth', authRouter);
-apiRouter.use('/users', userRouter);
+
+apiRouter.use("/auth", authRouter);
+apiRouter.use("/users", userRouter);
 apiRouter.use("/playthrough", playthroughRouter);
+apiRouter.use("/choice", choiceRouter);
 
 export default apiRouter;
