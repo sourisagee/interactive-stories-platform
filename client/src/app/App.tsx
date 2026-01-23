542331340b0d4464bfc8f6680d9245f3b011b6f3
Dende98 <danilopalov@gmail.com>
2026-01-23 12:58:24 +0300

    Profile_Demo

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -8,6 +8,7 @@ import MainPage from "../pages/MainPage/MainPage";
 import { useAppDispatch } from "../shared/hooks/reduxHooks";
 import { refreshThunk } from "../entities/user/api/UserApi";
 import { CLIENT_ROUTES } from "../shared/enam/clientRouter";
+import ProfilePage from "@/pages/ProfilePage/ProfilePage"
 
 export default function App() {
   const dispatch = useAppDispatch();
@@ -24,6 +25,7 @@ export default function App() {
         <Route path={CLIENT_ROUTES.SIGN_UP} element={<SignUpPage />} />
         <Route path={CLIENT_ROUTES.SIGN_IN} element={<SignInPage />} />
         <Route path={CLIENT_ROUTES.SIGN_OUT} element={<SignOutPage />} />
+        <Route path={CLIENT_ROUTES.PROFILE} element ={<ProfilePage/>}/>
       </Routes>
     </BrowserRouter>
   );
